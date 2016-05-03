02fb1ea80392ee2ac785114c570ac583ac7a4bfc
Bhuvaneshwari <bhuvaneshwari.tsm@gmail.com>
2016-05-03 22:13:42 +0530

    nodejs server

--- nodejs/hello.js
+++ nodejs/hello.js
@@ -1,3 +1,11 @@
 var js = 'hello';
 console.log(js);
 console.log('nodejs');
+const http=require('http');
+
+http.createServer((request,response)=>{
+  response.writeHead(200,{'Content-Type':'text/plain'});
+  response.end('Hello World\n');
+}).listen(8124);
+
+console.log('server running at http://127.0.0.1:8124');
